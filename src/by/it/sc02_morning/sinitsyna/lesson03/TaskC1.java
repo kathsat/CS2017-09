238d8b438bbb4b59f2f1dbebcc72440dacd68112
marina <sma_86@list.ru>
2017-10-03 12:26:40 +0300

    b1

--- src/by/it/sc02_morning/sinitsyna/lesson03/TaskC1.java
+++ src/by/it/sc02_morning/sinitsyna/lesson03/TaskC1.java
@@ -1,7 +1,5 @@
 package by.it.sc02_morning.sinitsyna.lesson03;
 
-import java.util.Scanner;
-
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -12,11 +10,7 @@ import java.util.Scanner;
 Sum = 26
 */
 class TaskC1 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int res = a + b;
-        System.out.println("sum =" + res);
-    }
+
+
+
 }
