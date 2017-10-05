f5f9eae14add3f44892504438e5fd512c03d3870
Elena <serzhan.helen@gmail.com>
2017-10-05 11:28:24 +0300

    Task C2  complete

--- src/by/it/sc02_morning/serzhan/lesson03/TaskC2.java
+++ src/by/it/sc02_morning/serzhan/lesson03/TaskC2.java
@@ -18,22 +18,13 @@ OKT:42+32=74
 class TaskC2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int result = b + a;
-        String dec = "DEC:";
-        System.out.println(dec+ a+"+"+b+"="+result);
-        //String convert = Integer.toBinaryString(a);
-        //String conv = Integer.toBinaryString(b);
-        //String con = Integer.toBinaryString(result);
-        String bin = "BIN:";
-     System.out.println(Integer.toBinaryString(result));
-        //System.out.println(bin+a+"+"+b+"="+result);
-       // convert = Integer.toOctalString(result);
-     System.out.println(Integer.toOctalString(result));
-        //convert = Integer. toHexString(result);
-        System.out.println(Integer. toHexString(result));
-        //System.out.println(Integer.toString(BIN:  "result", 2));
+        int s1 = sc.nextInt();
+        int s2 = sc.nextInt();
+        int result = s1 + s2;
+        System.out.println("DEC:"+ s1+"+"+s2+"="+result);
+        System.out.println("BIN:"+Integer.toBinaryString(((int)(s1)))+"+"+Integer.toBinaryString((int)(s2))+"="+Integer.toBinaryString((int)(result)));
+        System.out.println("HEX:"+Integer.toHexString(((int)(s1)))+"+"+Integer.toHexString((int)(s2))+"="+Integer.toHexString((int)(result)));
+        System.out.println("OKT:"+Integer.toOctalString(((int)(s1)))+"+"+Integer.toOctalString((int)(s2))+"="+Integer.toOctalString((int)(result)));
     }
 
     }
