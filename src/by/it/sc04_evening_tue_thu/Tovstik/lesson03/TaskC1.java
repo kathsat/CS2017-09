86b92d82c866849059cd9606874f0c0b9074d754
MikhailTov <Mensk84@gmail.com>
2017-10-05 14:06:23 +0300

    lesson A1,2,3;B1,2,3

--- src/by/it/sc04_evening_tue_thu/Tovstik/lesson03/TaskC1.java
+++ src/by/it/sc04_evening_tue_thu/Tovstik/lesson03/TaskC1.java
@@ -14,6 +14,7 @@ Sum = 26
 class TaskC1 {
     public static void main(String[] args) {
         Scanner sc =  new Scanner(System.in);
+
         int a = sc.nextInt();
         int b = sc.nextInt();
         int result = a + b;
