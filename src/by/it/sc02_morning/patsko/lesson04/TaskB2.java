82d93bc221e1f84370aa762f1337eb3085b85280
PatskoArkadzi <arkpatsko@gmail.com>
2017-10-05 22:44:51 +0300

    Lesson 04 Tasks A,B,C
    Проблемы с проверкой TaskB2:
    
    корни уровнения 2x*x+5x+3=0 ,будут -1,0 и -1,5 как и указано на github:
    https://github.com/Khmelov/CS2017-09/blob/master/src/by/it/akhmelev/lesson04/TaskB2.java
    
    А при тестировании идет сравнение со значениями -4,75 и -5,25:
    java.lang.AssertionError: Строка не найдена: -4.75 -5.25
    
    testing = new Testing(TaskB2.class, "2 5 3");
            testing.contains("-4.75 -5.25\n");

---
+++ src/by/it/sc02_morning/patsko/lesson04/TaskB2.java
@@ -0,0 +1,51 @@
+package by.it.sc02_morning.patsko.lesson04;
+
+import java.util.Scanner;
+
+/*
+Lesson 04. Task B2. Нужно написать программу, которая вводит три числа  a b c типа int с клавиатуры
+и вычисляет корни квадратного уравнения a*a*x + b*x + c = 0:
+1) Если два корня, то выводится два корня через пробел.
+2) Если один корень, то выводится только одно число - корень.
+3) Если нет действительных корней, то выводится сообщение об ошибке "Отрицательный дискриминант"
+
+В программе должен быть определен статический метод dis вычисления дискриминанта,
+который принимает на входе три числа типа int a,b,c
+а на выходе возвращает число типа double
+
+Примеры работы
+
+Ввод:
+2 5 3
+Вывод:
+-4.75 -5.25
+
+Ввод:
+2 4 2
+Вывод:
+-1
+
+Ввод:
+2 2 2
+Вывод:
+Отрицательный дискриминант
+
+*/
+class TaskB2 {
+    static double dis(int i, int j, int k) {
+        return j*j-4*i*k;
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Введите a:");
+        int a=sc.nextInt();
+        System.out.println("Введите b:");
+        int b=sc.nextInt();
+        System.out.println("Введите c:");
+        int c=sc.nextInt();
+        double d=dis(a,b,c);
+        if(d>0) System.out.println((-b+Math.sqrt(d))/(2*a)+" "+(-b-Math.sqrt(d))/(2*a));
+        if(d==0) System.out.println(-b/(2*a));
+        if(d<0) System.out.println("Отрицательный дискриминант");
+    }
+}
