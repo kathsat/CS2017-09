2c50f46a002e22ab95fb9e5daba504ecf9f0efd3
shinkevich <vadimeno@yandex.ru>
2017-10-05 20:48:59 +0300

    AB

---
+++ src/by/it/sc04_evening_tue_thu/shinkevich/lesson03/TaskA1.java
@@ -0,0 +1,19 @@
+package by.it.sc04_evening_tue_thu.shinkevich.lesson03;
+
+class TaskA1
+{
+/*
+напишите программу которая выводит на экран
+Hello world!
+
+(строка должа быть одна, ошибки в тексте не допускаются)
+Для ручной проверки запустите программу Ctrl+Shift+F10
+Для автоматической проверки откройте и запустите класс Testing
+*/
+ private static String TEXT = "Hello world!";
+
+ public static void main(String[] args)
+    {
+      System.out.println(TEXT);
+    }
+}
