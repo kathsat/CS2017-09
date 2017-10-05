bbe717a8ac1e6561a631fae15ffa244a98c68c23
safronova <alexey.safronov96@gmail.com>
2017-10-06 02:17:05 +0300

    Safronov PS непонятные проблемы с А1

---
+++ src/by/it/sc02_morning/safronov/lesson04/TaskC2.java
@@ -0,0 +1,42 @@
+package by.it.sc02_morning.safronov.lesson04;
+/*
+    Сумма цифр четырехзначного числа
+
+    Реализуйте метод sumDigitsInNumber(int number).
+    Метод на вход принимает целое четырехзначное число.
+    Нужно посчитать сумму цифр этого числа, и вернуть эту сумму.
+
+    Пример:
+    Метод sumDigitsInNumber вызывается с параметром 5467.
+
+    Пример вывода:
+    22
+
+    Требования:
+    1. В методе sumDigitsInNumber не могут быть использованы никакие дополнительные классы.
+    2. Метод sumDigitsInNumber(int) должен быть не приватным и статическим.
+    3. Метод sumDigitsInNumber должен возвращать значение типа int.
+    4. Метод sumDigitsInNumber не должен ничего выводить на экран.
+    5. Метод sumDigitsInNumber должен правильно возвращать сумму всех цифр в числе number.
+*/
+public class TaskC2 {
+
+
+
+
+   public static void main(String[] args) {
+       System.out.println(sumDigitsInNumber(5467));
+   }
+
+   public static int sumDigitsInNumber(int number)
+   {
+       String[] array = Integer.toString(number).split("");
+       int sum = 0;
+       for (int i = 0; i < array.length; i++)
+       {
+           sum += Integer.parseInt(array[i]);
+       }
+       return sum;
+    }
+
+}
