1f49520e00bc0fd1ca0fc10c08c20829f5fbab4f
sweta <sakura211@tut.by>
2017-10-05 22:01:41 +0300

    Lesson 4: A B C are done with right testing!

--- src/by/it/sc02_morning/liplianina/lesson04/TaskB1.java
+++ src/by/it/sc02_morning/liplianina/lesson04/TaskB1.java
@@ -30,6 +30,7 @@ strictfp class TaskB1 {
         int o = 0111;
         int h = 0x111;
         float f = (float) 111.111;
+
         System.out.println(d + b + o + h + f);
         System.out.println(d + " " + b + " " + o + " " + h + " " + f);
     }
