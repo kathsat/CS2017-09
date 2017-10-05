69cab22d28950fa830f8f11c016d83c51f9f444d
jiayu <yaotiao94@gmail.com>
2017-10-05 15:12:06 +0300

    Task C3 done

--- src/by/it/sc04_evening_tue_thu/krasikova/lesson03/TaskC3.java
+++ src/by/it/sc04_evening_tue_thu/krasikova/lesson03/TaskC3.java
@@ -39,17 +39,18 @@ class TaskC3 {
         System.out.println(i);
         System.out.println("");
         System.out.println("Вывод:");
-        System.out.println(String.format("%(.2f", getWeight(i)));
+        System.out.println(getWeight(i));
 
     }
 
     public static double getWeight(double x) {
-        double weightMars;
-        weightMars= 3.86 / 9.81 * x;
-        return weightMars;
+        double weightMars = 3.86 / 9.81 * x * 100;
+        int weight = (int) weightMars + 1;
+        double finalWeightMars = (double) weight / 100;
+        return finalWeightMars;
 
     }
-    }
+}
 
 
 
