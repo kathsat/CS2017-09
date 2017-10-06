e13d38a4dbf41ef5cb0a0eb3cde1f9f07bef4b7d
passhub <lpv550@gmail.com>
2017-10-06 20:05:31 +0300

    AB + C(partly)

--- src/by/it/sc03_evening_mon_fri/liubimov_pavel/lesson03/TaskC1.java
+++ src/by/it/sc03_evening_mon_fri/liubimov_pavel/lesson03/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.sc03_evening_mon_fri.liubimov_pavel.lesson03;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -10,7 +12,16 @@ package by.it.sc03_evening_mon_fri.liubimov_pavel.lesson03;
 Sum = 26
 */
 class TaskC1 {
+  public static void main(String args[]) {
+      Scanner in = new Scanner(System.in);
+      System.out.println("Ввод:");
+      int i = in.nextInt();
+      int a = in.nextInt();
+      System.out.println("Вывод:");
+      System.out.println("Sum = " + (i + a) );
 
+
+}
 
 
 }
