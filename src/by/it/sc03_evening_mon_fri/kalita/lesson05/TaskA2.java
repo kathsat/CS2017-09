02e1d99a07a5ec407db5902177589a187fed9801
Denis <Buridza0@gmail.com>
2017-10-09 18:58:30 +0300

    Lesson 5, A2

--- src/by/it/sc03_evening_mon_fri/kalita/lesson05/TaskA2.java
+++ src/by/it/sc03_evening_mon_fri/kalita/lesson05/TaskA2.java
@@ -34,18 +34,19 @@ public class TaskA2 {
             switch (date){
             case 1:
                 System.out.println("Понедельник");
+                break;
                 case 2:
-                System.out.println("Вторник");
+                System.out.println("Вторник");break;
                 case 3:
-                System.out.println("Среда");
+                System.out.println("Среда");break;
                 case 4:
-                System.out.println("Четверг");
+                System.out.println("Четверг");break;
                 case 5:
-                System.out.println("Пятница");
+                System.out.println("Пятница");break;
                 case 6:
-                System.out.println("Суббота");
+                System.out.println("Суббота");break;
                 case 7:
-                System.out.println("Воскресенье");
+                System.out.println("Воскресенье");break;
             }
          else System.out.println("Такого дня недели не существует");
 
