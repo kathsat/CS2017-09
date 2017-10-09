652f2b48a063ecd914cf259e715bdeadf35dc009
Victoria <travel.vi@mail.ru>
2017-10-09 19:11:47 +0300

    Lesson 4 Complete All tasks Task C1 is fixed

--- src/by/it/sc04_evening_tue_thu/runec/lesson04/TaskB1.java
+++ src/by/it/sc04_evening_tue_thu/runec/lesson04/TaskB1.java
@@ -23,13 +23,14 @@ float f - присваивание в десятичном формате
 Для автоматической проверки откройте и запустите класс Testing
 */
 
-strictfp class TaskB1 {
+class TaskB1 {
     public static  void main(String[] args) {
-        double d = 0;
-        double b = 0;
-        double o = 0;
-        double h = 0;
-        double f = 0;
+        double d = 111.111;
+        byte b = 0b111;
+        long o = 0111;
+        int h = 0x111;
+        float f = 111.111f;
+
         System.out.println(d + b + o + h + f);
         System.out.println(d + " " + b + " " + o + " " + h + " " + f);
     }
