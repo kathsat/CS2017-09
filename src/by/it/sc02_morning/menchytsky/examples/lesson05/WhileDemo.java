d9fe54c9519afb82871c9c163b485064e4abce27
Stepan <s.menchytsky@gmail.com>
2017-10-09 17:40:13 +0300

    A1-2, B1-2 complete.

---
+++ src/by/it/sc02_morning/menchytsky/examples/lesson05/WhileDemo.java
@@ -0,0 +1,15 @@
+package by.it.sc02_morning.menchytsky.examples.lesson05;
+
+// Demonstrate the while loop.
+class WhileDemo {   
+  public static void main(String args[]) { 
+    char ch; 
+ 
+    // print the alphabet using a while loop 
+    ch = 'a'; 
+    while(ch <= 'z') { 
+      System.out.print(ch); 
+      ch++; 
+    } 
+  }   
+}
