474a9777fc2ec6022881a8c620929c383bdbd60c
jiayu <yaotiao94@gmail.com>
2017-10-09 20:53:11 +0300

    A1 - C2 done

--- src/by/it/sc04_evening_tue_thu/krasikova/lesson04/TaskC2.java
+++ src/by/it/sc04_evening_tue_thu/krasikova/lesson04/TaskC2.java
@@ -1,4 +1,5 @@
 package by.it.sc04_evening_tue_thu.krasikova.lesson04;
+
 /*
     Сумма цифр четырехзначного числа
 
@@ -19,13 +20,19 @@ package by.it.sc04_evening_tue_thu.krasikova.lesson04;
     4. Метод sumDigitsInNumber не должен ничего выводить на экран.
     5. Метод sumDigitsInNumber должен правильно возвращать сумму всех цифр в числе number.
 */
+
+import java.util.Scanner;
+
 public class TaskC2 {
+    public static int sumDigitsInNumber(int number) {
+        int result = (number / 1000) + ((number % 1000) / 100) + (((number % 1000) % 100) / 10) + ((((number % 1000) % 100) % 10));
+        return result;
+    }
 
-
-
-
-//    public static void main(String[] args) {
-//        System.out.println(sumDigitsInNumber(5467));
-//    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(sumDigitsInNumber(i));
+    }
 
 }
