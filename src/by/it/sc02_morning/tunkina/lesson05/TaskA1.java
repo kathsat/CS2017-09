7794ea56a3284057e05fa697346a72bb47a3c640
tunkina <3217813@gmail.com>
2017-10-10 12:14:17 +0300

    lesson 5

--- src/by/it/sc02_morning/tunkina/lesson05/TaskA1.java
+++ src/by/it/sc02_morning/tunkina/lesson05/TaskA1.java
@@ -25,5 +25,15 @@ package by.it.sc02_morning.tunkina.lesson05;
 
  */
 public class TaskA1 {
+    public static void main(String[] args) {
+        int x;
+        for ( x = 1; x <= 10; x++) {
+            if (x == 3 || x== 9 ) System.out.println("Меня интересует высокая зарплата!");
+                         else   System.out.println("Я изучаю программирование.");
+
+        }
+
+
+    }
 
 }
