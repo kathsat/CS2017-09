8c77d1d7d9066814fe934119d3b0035f6d6c510d
jiayu <yaotiao94@gmail.com>
2017-10-10 21:05:44 +0300

    A1 - A3 done

--- src/by/it/sc04_evening_tue_thu/krasikova/lesson05/TaskA3.java
+++ src/by/it/sc04_evening_tue_thu/krasikova/lesson05/TaskA3.java
@@ -21,7 +21,24 @@ package by.it.sc04_evening_tue_thu.krasikova.lesson05;
 
  */
 
+import java.util.Scanner;
 
 public class TaskA3 {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if (a == b && a == c) {
+            System.out.println(a + " " + b + " " + c);
+        } else if (a == b) {
+            System.out.println(a+" "+b);
+        } else if (a == c) {
+            System.out.println(a+" "+c);
+        } else if (b == c) {
+            System.out.println(b+" "+c);
+        }
 
+    }
 }
+
