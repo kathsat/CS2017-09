b913aaa9c59f65d23dd02090819ebecfa6f1a670
gimzhevsky <gimvit@gmail.com>
2017-10-10 16:54:05 +0300

    Lesson 4, Task A1-C2

---
+++ src/by/it/sc04_evening_tue_thu/gimzhevsky/lesson04/TaskC2.java
@@ -0,0 +1,51 @@
+package by.it.sc04_evening_tue_thu.gimzhevsky.lesson04;
+/*
+    Сумма цифр четырехзначного числа
+
+    Реализуйте метод sumDigitsInNumber(int number).
+    Метод на вход принимает целое четырехзначное число.
+    Нужно посчитать сумму цифр этого числа, и вернуть эту сумму.
+
+    Пример:
+    Метод sumDigitsInNumber вызывается с параметром 5467.
+
+    Пример вывода:
+    22
+
+    Требования:
+    1. В методе sumDigitsInNumber не могут быть использованы никакие дополнительные классы.
+    2. Метод sumDigitsInNumber(int) должен быть не приватным и статическим.
+    3. Метод sumDigitsInNumber должен возвращать значение типа int.
+    4. Метод sumDigitsInNumber не должен ничего выводить на экран.
+    5. Метод sumDigitsInNumber должен правильно возвращать сумму всех цифр в числе number.
+*/
+public class TaskC2 {
+   /* public int sumDigitsInNumber(int number){
+       int sum = 0;
+        int a = number%10;
+        number = (number/10);
+        sum = sum+a;
+        System.out.print(number);
+        System.out.print(a);
+        System.out.println(sum);
+
+
+
+        return (sum);
+        */
+        /* int sum = sum + i%10;
+        System.out.println("sum - "+sum);
+        int val = i/10;
+        if (val >=10) {
+            sumDigitsInNumber(val);
+            System.out.println(val);
+            return sum;
+
+    }
+
+
+   public static void main(String[] args) {
+       System.out.println(sumDigitsInNumber(5467));
+
+    }*/
+}
