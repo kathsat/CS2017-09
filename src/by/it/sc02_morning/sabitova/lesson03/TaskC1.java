bbcc9f7ecd3a39cef6111a2a3330696fb38eb8a4
SvetlanaSabitova <svlanasabitova@gmail.com>
2017-10-10 11:07:09 +0300

    First commit

---
+++ src/by/it/sc02_morning/sabitova/lesson03/TaskC1.java
@@ -0,0 +1,24 @@
+package by.it.sc02_morning.sabitova.lesson03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/* Нужно написать программу, которая вводит два числа с клавиатуры
+и выводит их сумму на экран в виде
+
+Ввод:
+34 26
+
+Вывод:
+Sum = 26
+*/
+class TaskC1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int y=sc.nextInt();
+        System.out.println("Sum"+" = "+(x+y));
+        }
+    }
+
