1aed9c07e432368ab817a966d008788ad9e0b985
Mary <lutskevichmaria@gmail.ru>
2017-10-10 22:56:05 +0300

    Lesson05.Tasks A1-B3, C2

---
+++ src/by/it/sc02_morning/lutskevichm/lesson05/TaskC2.java
@@ -0,0 +1,74 @@
+package by.it.sc02_morning.lutskevichm.lesson05;
+
+import java.util.Scanner;
+
+/*
+На входе вводится число и месяц 2017 года.
+Программа должна выводить номер дня года, считая 1 января первым днем,
+а 31 декабря 365-м.
+
+Пример:
+Ввод:
+1 1
+
+Вывод
+1
+
+Ввод:
+31 12
+
+Вывод
+365
+
+Ввод:
+13 9
+
+Вывод
+256
+
+ */
+public class TaskC2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int i=sc.nextInt();
+        int j=sc.nextInt();
+        switch (j) {
+            case 1:
+                System.out.println(i);
+                break;
+            case 2:
+                System.out.println(31+i);
+                break;
+            case 3:
+                System.out.println(31+28+i);
+                break;
+            case 4:
+                System.out.println(31*2+28+i);
+                break;
+            case 5:
+                System.out.println(31*2+28+30+i);
+                break;
+            case 6:
+                System.out.println(31*3+28+30+i);
+                break;
+            case 7:
+                System.out.println(31*3+28+30*2+i);
+                break;
+            case 8:
+                System.out.println(31*4+28+30*2+i);
+                break;
+            case 9:
+                System.out.println(31*5+28+30*2+i);
+                break;
+            case 10:
+                System.out.println(31*5+28+30*3+i);
+                break;
+            case 11:
+                System.out.println(31*6+28+30*3+i);
+                break;
+            case 12:
+                System.out.println(31*6+28+30*4+i);
+                break;
+        }
+    }
+}
