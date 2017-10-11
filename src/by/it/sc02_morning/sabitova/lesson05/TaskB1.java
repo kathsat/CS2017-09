09bf995a54926f59af06f8d160b3a3b4c356319b
SvetlanaSabitova <20032003mn>
2017-10-11 19:39:51 +0300

    lesson 5

--- src/by/it/sc02_morning/sabitova/lesson05/TaskB1.java
+++ src/by/it/sc02_morning/sabitova/lesson05/TaskB1.java
@@ -1,5 +1,7 @@
 package by.it.sc02_morning.sabitova.lesson05;
 
+import java.util.Scanner;
+
 /*
 Определите количество дней в году
 
@@ -26,5 +28,17 @@ package by.it.sc02_morning.sabitova.lesson05;
 
 */
 public class TaskB1 {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int year = scanner.nextInt();
+        if ((year % 4) == 0) {
+            if ((year % 100) == 0 && (year % 400) != 0) {
+                System.out.println("количество дней в году: 365");
+            } else {
+                System.out.println("количество дней в году: 366");
+            }
+        } else {
+            System.out.println("количество дней в году: 365");
+        }
+    }
 }
