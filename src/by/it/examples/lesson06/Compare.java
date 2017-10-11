add7ec1b988db7dd2073aa99e2b2ad6ab64fc2bb
Khmelov <375336849110@tut.by>
2017-10-12 01:41:48 +0300

    Lesson06 Tasks complete with examples

---
+++ src/by/it/examples/lesson06/Compare.java
@@ -0,0 +1,14 @@
+package by.it.examples.lesson06;
+
+public class Compare {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 20;
+        System.out.println("a == b = " + (a == b) );
+        System.out.println("a != b = " + (a != b) );
+        System.out.println("a > b = " + (a > b) );
+        System.out.println("a < b = " + (a < b) );
+        System.out.println("b >= a = " + (b >= a) );
+        System.out.println("b <= a = " + (b <= a) );
+    }
+}
---
+++ src/by/it/examples/lesson06/While.java
@@ -0,0 +1,13 @@
+package by.it.examples.lesson06;
+
+public class While {
+    public static void main(String args[]) {
+        int x = 10;
+
+        while( x < 15 ) {
+            System.out.print("Значение x: " + x );
+            x++;
+            System.out.print("\n");
+        }
+    }
+}
