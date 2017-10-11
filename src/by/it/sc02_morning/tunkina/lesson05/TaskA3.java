739d5911716d04fe0ac4e690168d720e18aaa57e
tunkina <3217813@gmail.com>
2017-10-11 23:33:54 +0300

    lesson 5

--- src/by/it/sc02_morning/tunkina/lesson05/TaskA3.java
+++ src/by/it/sc02_morning/tunkina/lesson05/TaskA3.java
@@ -22,6 +22,24 @@ package by.it.sc02_morning.tunkina.lesson05;
  */
 
 
+import java.util.Scanner;
+
 public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(a==b & a==c)
+            System.out.println(a+" "+b+" "+c);
+
+       else  if (a==b)
+            System.out.println(a+" "+b);
+       else if (a==c)
+            System.out.println(a+" "+c);
+       else if (b==c)
+            System.out.println(b+" "+c);
+
+    }
 
 }
