dcf7959ab780e6832729ba2e3fee5b500677be76
Denis <Buridza0@gmail.com>
2017-10-12 12:38:20 +0300

    Lesson 6, complete A3

--- src/by/it/sc03_evening_mon_fri/kalita/lesson06/TaskA3.java
+++ src/by/it/sc03_evening_mon_fri/kalita/lesson06/TaskA3.java
@@ -21,6 +21,20 @@ ABCDEFGHI
 ABCDEFGHIJ
 */
 
+import java.util.Scanner;
+
 public class TaskA3 {
+    public static void main(String[] args) {
+       /* for (int i = 0; i < 256;) {
+            System.out.println(i+": "+(char)i+++"  "+i+": "+(char)i++);
+        }*/
+        for (int i = 1, num = 65, size = new Scanner(System.in).nextInt(); i < size+1; i++,num=65) {
+            for (int j = 0; j < i; j++) {
+                System.out.print((char)num++);
+            }
+            System.out.println();
+
+        }
+    }
 
 }
