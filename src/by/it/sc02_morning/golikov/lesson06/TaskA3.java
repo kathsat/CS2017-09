0dd438466ff1a12d2258dba05e0fd08c8e0f94fc
vgolikov <vvgolikov7@gmail.com>
2017-10-12 14:55:14 +0300

    A1, A2 completed

---
+++ src/by/it/sc02_morning/golikov/lesson06/TaskA3.java
@@ -0,0 +1,26 @@
+package by.it.sc02_morning.golikov.lesson06;
+/*
+Треугольник из $
+Прочитать переменную size с клавиатуры
+
+Используя цикл for вывести на экран
+прямоугольный треугольник из букв алфавита (ENGLISH), начиная с буквы A со сторонами size * size.
+
+Пример
+Ввод: 10
+Вывод:
+A
+AB
+ABC
+ABCD
+ABCDE
+ABCDEF
+ABCDEFG
+ABCDEFGH
+ABCDEFGHI
+ABCDEFGHIJ
+*/
+
+public class TaskA3 {
+
+}
