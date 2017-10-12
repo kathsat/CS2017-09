e6bfee277b10a962f48b598395971541822e0359
Mary <lutskevichmaria@gmail.ru>
2017-10-12 23:12:23 +0300

    Lesson06.Tasks A1-A3

---
+++ src/by/it/sc02_morning/lutskevichm/lesson06/TaskA3.java
@@ -0,0 +1,39 @@
+package by.it.sc02_morning.lutskevichm.lesson06;
+/*
+Треугольник из $
+Прочитать переменную size с клавиатуры
+
+Используя цикл for вывести на экран
+прямоугольный треугольник из букв алфавита (ENGLISH), начиная с буквы A со сторонами size * size.
+
+Пример
+Ввод: 10
+Вывод:
+$
+$$
+$$$
+$$$$
+$$$$$
+$$$$$$
+$$$$$$$
+$$$$$$$$
+$$$$$$$$$
+$$$$$$$$$$
+
+*/
+
+import java.util.Scanner;
+
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int size=sc.nextInt();
+        char ch ='A';
+        for (int i=0; i<size;i++) {
+            for (int j =0; j <= i; j++) {
+                System.out.print((char)(ch + j));
+            }
+            System.out.println();
+        }
+    }
+}
