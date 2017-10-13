10c83746ba98a259e91d81e500d9bd951884b7e5
halaevanika <halaeva.nika@gmail.com>
2017-10-13 20:23:01 +0300

    Lesson05

--- src/by/it/sc03_evening_mon_fri/halaevanika/lesson03/TaskA2.java
+++ src/by/it/sc03_evening_mon_fri/halaevanika/lesson03/TaskA2.java
@@ -1,4 +1,4 @@
-package by.it.sc03_evening_mon_fri.halaevanika.lesson03.lesson03;
+package by.it.sc03_evening_mon_fri.halaevanika.lesson03;
 
 /*
 напишите тело main программы которая выводит на экран 5 строк
--- src/by/it/sc03_evening_mon_fri/halaevanika/lesson03/TaskB2.java
+++ src/by/it/sc03_evening_mon_fri/halaevanika/lesson03/TaskB2.java
@@ -1,4 +1,4 @@
-package by.it.sc03_evening_mon_fri.halaevanika.lesson03.lesson03;
+package by.it.sc03_evening_mon_fri.halaevanika.lesson03;
 /*
 Подойдет только 20
 В методе main расставь правильно знаки плюс и минус, чтобы значение переменной result получилось равным 20.
