1b47fb13f6ae547f8f8816d46f6f11fae39206aa
Denis <Buridza0@gmail.com>
2017-10-13 20:49:19 +0300

    Lesson 6, complete C2

--- src/by/it/sc03_evening_mon_fri/kalita/lesson06/TaskC2.java
+++ src/by/it/sc03_evening_mon_fri/kalita/lesson06/TaskC2.java
@@ -17,5 +17,32 @@ package by.it.sc03_evening_mon_fri.kalita.lesson06;
 
  */
 public class TaskC2 {
+    public static long fact(int k){
+        long max=1L;
+        long out=0;
+        for (int i=1;; i++) {
+
+            /*if(Long.MAX_VALUE/2>Long.MAX_VALUE-max){
+                break;
+            }*/
+            if(max<0)break;
+
+           // System.out.println(max + " " +i);
+            out=max;
+            max*=i;
+        }
+        return out;
+    }
+    public static void main(String[] args) {
+      int k=1;
+      long l=1L;
+      int i=1;
+       /* for (;i<30; i++) {
+            k*=i;
+            System.out.println(k+" "+i);
+        }*/
+        //fact(k)
+        System.out.printf("%d=%x=%s", fact(k), fact(k), Long.toBinaryString(fact(k)));
+    }
 
 }
