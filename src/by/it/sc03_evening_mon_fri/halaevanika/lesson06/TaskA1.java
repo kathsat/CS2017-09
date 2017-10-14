de0d41fb4c4ab1f876a3c5a4309d24ad5c0a19aa
halaevanika <halaeva.nika@gmail.com>
2017-10-14 15:57:17 +0300

    Lesson05

--- src/by/it/sc03_evening_mon_fri/halaevanika/lesson06/TaskA1.java
+++ src/by/it/sc03_evening_mon_fri/halaevanika/lesson06/TaskA1.java
@@ -23,11 +23,12 @@ $ $ $ $ $ $ $ $ $ $
 public class TaskA1 {
     public static void main(String[] args) {
        int i = 0;
-        while (i <=10){
+        while (i <100){
             System.out.print("$ ");
             i++;
-            if (i == 10)
+            if (i%10 == 0)
                 System.out.println();
+            continue;
         }
     }
 }
