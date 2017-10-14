a2be5b79447402de5f042d7948ca3ced7f39c702
halaevanika <halaeva.nika@gmail.com>
2017-10-14 20:27:45 +0300

    Lesson05

--- src/by/it/sc03_evening_mon_fri/halaevanika/lesson06/TaskA2.java
+++ src/by/it/sc03_evening_mon_fri/halaevanika/lesson06/TaskA2.java
@@ -16,6 +16,20 @@ $$$$
 
  */
 
+import java.util.Scanner;
+
 public class TaskA2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+      int m = sc.nextInt();
+       int n = sc.nextInt();
+        sc.close();
+        for (int i=0; i<m; i++){
+        for (int j=0; j<n; j++){
+            System.out.print("$");
+        }
+            System.out.println();
+        }
+    }
 
 }
