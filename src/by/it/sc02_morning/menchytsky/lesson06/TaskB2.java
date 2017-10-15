ecd82efd8718be568a135d823bf19409089df356
Stepan <s.menchytsky@gmail.com>
2017-10-15 22:08:15 +0300

    lesson06 Tasks B1,2  complete

--- src/by/it/sc02_morning/menchytsky/lesson06/TaskB2.java
+++ src/by/it/sc02_morning/menchytsky/lesson06/TaskB2.java
@@ -23,13 +23,29 @@ public class TaskB2 {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int a = Integer.parseInt(reader.readLine());
         int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        int d = Integer.parseInt(reader.readLine());
+        int e = Integer.parseInt(reader.readLine());
 
-        int minimum = min(a, b);
+        int minimum = min(a, b, c, d, e);
 
         System.out.println("Minimum = " + minimum);
     }
 
-    private static int min(int a, int b) {
-        return a < b ? a : b;
+    private static int min(int a, int b, int c, int d, int e) {
+       int g=0;
+        if(a<=b && a<=c && a<=d && a<=e)
+             g=a;
+        else if(b<=a && b<=c && b<=d && b<=e)
+            g= b;
+        else if(c<=a && c<=b && c<=d && c<=e)
+        g =c;
+        else if(d<=a && d<=b && d<=c && d<=e)
+            g = d;
+        else if(e<=a && e<=b && e<=c && e<=d)
+       g = e;
+
+        return g;
+
     }
 }
