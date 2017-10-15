b46026a7394b05279f90363c890279786af9a17d
sushchenja <denis.sushchenja@gmail.com>
2017-10-15 19:34:10 +0300

    a1-a2

---
+++ src/by/it/sc04_evening_tue_thu/sushchenja/lesson06/TaskA3.java
@@ -0,0 +1,28 @@
+package by.it.sc04_evening_tue_thu.sushchenja.lesson06;
+/*
+Треугольник из $
+Прочитать переменную size с клавиатуры
+
+Используя цикл for вывести на экран
+прямоугольный треугольник из букв алфавита (ENGLISH), начиная с буквы A со сторонами size * size.
+
+Пример
+Ввод: 10
+Вывод:
+A
+AB
+ABC
+ABCD
+ABCDE
+ABCDEF
+ABCDEFG
+ABCDEFGH
+ABCDEFGHI
+ABCDEFGHIJ
+*/
+
+import java.util.Scanner;
+
+public class TaskA3 {
+
+}
