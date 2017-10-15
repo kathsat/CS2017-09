76bd10339568787ef891176fbf7628c90dc0365f
Stepan <s.menchytsky@gmail.com>
2017-10-15 19:39:47 +0300

    lesson06 Tasks A  complete

--- src/by/it/sc02_morning/menchytsky/lesson06/TaskA2.java
+++ src/by/it/sc02_morning/menchytsky/lesson06/TaskA2.java
@@ -16,6 +16,21 @@ $$$$
 
  */
 
+import java.util.Scanner;
+
 public class TaskA2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+
+        for(int i=1 ; i<=m ; i++){
+            for(int y=1 ; y<=n ; y++){
+                System.out.print("$");
+            }
+            System.out.println();
+        }
+
+    }
 
 }
