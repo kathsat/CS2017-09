d89c3b39d75c61b914da666611fcd7303b8bf956
NM <natalia.motuz@gmail.com>
2017-10-15 23:41:07 +0300

    commit 2, 15.10.2017

--- src/by/it/sc04_evening_tue_thu/motuz/lesson03/TaskC1.java
+++ src/by/it/sc04_evening_tue_thu/motuz/lesson03/TaskC1.java
@@ -1,5 +1,7 @@
 package by.it.sc04_evening_tue_thu.motuz.lesson03;
 
+import java.util.Scanner;
+
 /* Нужно написать программу, которая вводит два числа с клавиатуры
 и выводит их сумму на экран в виде
 
@@ -10,7 +12,12 @@ package by.it.sc04_evening_tue_thu.motuz.lesson03;
 Sum = 26
 */
 class TaskC1 {
-
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
 
 }
