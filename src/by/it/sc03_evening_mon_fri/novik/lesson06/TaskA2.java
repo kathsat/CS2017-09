39b4707349e458ae0a51e41c8581005b532c8e53
alexnovikdev <alexnovikdev@gmail.com>
2017-10-16 17:03:28 +0300

    Lesson6 A, B, C

---
+++ src/by/it/sc03_evening_mon_fri/novik/lesson06/TaskA2.java
@@ -0,0 +1,44 @@
+package by.it.sc03_evening_mon_fri.novik.lesson06;
+/*
+Рисуем долларовый прямоугольник
+Ввести с клавиатуры два числа m и n.
+Используя цикл for вывести на экран прямоугольник высотой m и шириной n из знаков $.
+
+Пример: m=2, n=4
+$$$$
+$$$$
+
+Требования:
+1. Программа должна считывать числа c клавиатуры.
+2. Программа должна выводить числа на экран.
+3. Программа должна выводить прямоугольник размером m на n из знаков $.
+4. В программе должен использоваться цикл for.
+
+ */
+
+import java.util.Scanner;
+
+public class TaskA2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int m = scanner.nextInt();
+        int n = scanner.nextInt();
+
+        for (int i = 0; i < m; i++) {
+
+            for (int j = 0; j < n; j++) {
+
+                System.out.print("$");
+
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
---
+++ src/by/it/sc03_evening_mon_fri/novik/lesson06/TaskB2.java
@@ -0,0 +1,48 @@
+package by.it.sc03_evening_mon_fri.novik.lesson06;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+/*
+Новый способ ввода данных + поиск минимума
+
+Текущая реализация:
+Программа вводит два числа с клавиатуры и выводит минимальное из них на экран.
+
+Новая задача:
+Программа вводит пять чисел с клавиатуры (через Enter) и выводит минимальное из них на экран.
+
+Требования:
+1. Программа должна считывать числа с клавиатуры.
+2. Программа должна выводить строку, которая начинается на "Minimum = ".
+3. Программа должна выводить строку, которая заканчивается минимальным из 5 введенных чисел.
+*/
+
+public class TaskB2 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] arrOfNum = new int[5];
+
+
+        for (int i = 0; i < 5; i++) {
+
+            arrOfNum[i] = Integer.parseInt(reader.readLine());
+
+        }
+
+        int minimum = arrOfNum[0];
+
+        for (int curNum : arrOfNum) {
+
+            minimum = min(minimum, curNum);
+
+        }
+
+        System.out.println("Minimum = " + minimum);
+    }
+
+    private static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+}
