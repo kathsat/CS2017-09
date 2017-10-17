f1a6f35535efccef9feee6c6d2f0dbd8bc9fb9d0
Marry <krawchinskaja@gmail.com>
2017-10-17 16:53:47 +0300

    A B C

---
+++ src/by/it/sc03_evening_mon_fri/kravchinskaja_/lesson06/TaskA3.java
@@ -0,0 +1,35 @@
+package by.it.sc03_evening_mon_fri.kravchinskaja_.lesson06;
+/*
+Треугольник из $
+Прочитать переменную size с клавиатуры
+
+Используя цикл for вывести на экран
+прямоугольный треугольник из букв алфавита (ENGLISH), начиная с буквы A со сторонами size * size.
+
+Пример
+Ввод: 10
+Вывод:
+A
+AB
+ABC
+ABCD
+ABCDE
+ABCDEF
+ABCDEFG
+ABCDEFGH
+ABCDEFGHI
+ABCDEFGHIJ
+*/
+
+
+import java.util.Scanner;
+
+public class TaskA3 {
+
+    public static void main(String[] args) {
+        Scanner cr = new Scanner(System.in);
+        int size = cr.nextInt();
+
+}
+
+}
