b8ca32c18a8f9991f0b542451afc35452baae88a
crescent9212 <metalllesta@gmail.com>
2017-10-17 14:10:54 +0300

    Lesson 6 A1-B3

---
+++ src/by/it/sc04_evening_tue_thu/migdasyova/lesson06/TaskB1.java
@@ -0,0 +1,34 @@
+package by.it.sc04_evening_tue_thu.migdasyova.lesson06;
+/*
+Суммирование
+Вводить с клавиатуры числа и считать их сумму.
+Если пользователь ввел -1, вывести на экран сумму и завершить программу.
+-1 должно тоже учитываться в сумме.
+
+
+Требования:
+1. Программа должна считывать числа c клавиатуры.
+2. Программа должна выводить число на экран.
+3. Программа должна посчитать сумму введенных чисел.
+Если пользователь ввел -1, вывести на экран сумму и завершить программу.
+4. В программе должен использоваться цикл for или цикл while.
+*/
+
+
+import java.util.Scanner;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+
+        while (true) {
+            int number = sc.nextInt();
+            sum += number;
+            if (number == -1) {
+                System.out.println(sum);
+                break;
+            }
+        }
+    }
+}
