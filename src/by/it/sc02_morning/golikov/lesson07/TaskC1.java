10aa0db86d9c75a4b3c0933a97c20c2dc53d6fac
vgolikov <vvgolikov7@gmail.com>
2017-10-18 13:14:04 +0300

    ALL completed

--- src/by/it/sc02_morning/golikov/lesson07/TaskC1.java
+++ src/by/it/sc02_morning/golikov/lesson07/TaskC1.java
@@ -10,14 +10,35 @@ package by.it.sc02_morning.golikov.lesson07;
     Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
 */
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class TaskC1 {
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
         Scanner sc = new Scanner(System.in);
-
+        ArrayList<Integer> list0 = new ArrayList<>();
+        ArrayList<Integer> list1 = new ArrayList<>();
+        ArrayList<Integer> list2 = new ArrayList<>();
+        ArrayList<Integer> list3 = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list0.add(sc.nextInt());
+        }
+        for (int i = 0; i < list0.size(); i++) {
+            if (list0.get(i) % 2 == 0)
+                list1.add(list0.get(i));
+            if (list0.get(i) % 3 == 0)
+                list2.add(list0.get(i));
+            if (list0.get(i) % 2 != 0 && list0.get(i) % 3 != 0)
+                list3.add(list0.get(i));
+//            if (list0.get(i) % 2 == 0 && list0.get(i) % 3 == 0) {
+//                list1.add(list0.get(i));
+//                list2.add(list0.get(i));
+//            }
+        }
+        printList(list2);
+        printList(list1);
+        printList(list3);
 
     }
 
