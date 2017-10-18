909287aae4461655be9aa1c397b8d51af5166a13
jiayu <yaotiao94@gmail.com>
2017-10-18 22:52:07 +0300

    Lesson 7 done

--- src/by/it/sc04_evening_tue_thu/krasikova/lesson07/TaskC2.java
+++ src/by/it/sc04_evening_tue_thu/krasikova/lesson07/TaskC2.java
@@ -3,16 +3,31 @@ package by.it.sc04_evening_tue_thu.krasikova.lesson07;
 /* Задачка на сортировку
 Задача: Написать программу, которая вводит с клавиатуры 20 чисел и выводит их в убывающем порядке.
 */
+import java.util.Scanner;
 
 public class TaskC2 {
-    //Напишите тут ваш код
-
-
-
-
+    public static void main (String args[]) {
+        Scanner sc=new Scanner(System.in);
+        int prog[]= new int[20];
+        for (int i=0; i < prog.length; i++) {
+            int l = sc.nextInt();
+            prog[i]=l;
+        }
+        sort(prog);}
 
     public static void sort(int[] array) {
-        //Напишите тут ваш код
+        for (int a=1; a < array.length; a++) {
+            for (int b = array.length-1; b >= a; b--) {
+                if (array[b-1] < array[b]) {
+                    int t= array[b-1];
+                    array[b-1]=array[b];
+                    array[b]=t;
+                }
+            }
+        }
+        for (int x : array) {
+            System.out.println(x);
+        }
     }
 
 }
