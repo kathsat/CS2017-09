5fcc33166978ec323a3f4c74cfd81d2dc1705af3
jiayu <yaotiao94@gmail.com>
2017-10-18 19:16:33 +0300

    A1 - B1 done

--- src/by/it/sc04_evening_tue_thu/krasikova/lesson07/TaskB1.java
+++ src/by/it/sc04_evening_tue_thu/krasikova/lesson07/TaskB1.java
@@ -9,20 +9,27 @@ package by.it.sc04_evening_tue_thu.krasikova.lesson07;
 
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.util.ArrayList;
 
 
 public class TaskB1 {
-    public static void main(String args[]) {
+    public static void main(String args[])  throws IOException {
         Reader r = new InputStreamReader(System.in);
         BufferedReader reader = new BufferedReader(r);
-        
 
         ArrayList<String> list=new ArrayList();
+        for (int i=0; i < 5; i++) {
+            String s = reader.readLine();
+            list.add(s);
+        }
+        System.out.println(list.size());
+        for (String i : list) {
+            System.out.println(i);
 
-
+        }
 
     }
 
