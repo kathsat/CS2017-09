08868669fe8921ad4dc20a2dc7448143bdd69ea9
halaevanika <halaeva.nika@gmail.com>
2017-10-18 19:07:05 +0300

    Lesson05

--- src/by/it/sc03_evening_mon_fri/halaevanika/lesson06/TaskA3.java
+++ src/by/it/sc03_evening_mon_fri/halaevanika/lesson06/TaskA3.java
@@ -21,6 +21,23 @@ ABCDEFGHI
 ABCDEFGHIJ
 */
 
-public class TaskA3 {
+import java.util.Scanner;
 
+public class TaskA3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int size = sc.nextInt();
+        char с='A';
+        for (int i=1; i<=size; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                System.out.print(с);
+                с++;
+            }
+            System.out.println("");
+            с='A';
+        }
+
+    }
 }
