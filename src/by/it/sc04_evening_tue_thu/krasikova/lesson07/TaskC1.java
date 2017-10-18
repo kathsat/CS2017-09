6c02b335def26c4f59ab23462260515be684973b
jiayu <yaotiao94@gmail.com>
2017-10-18 20:39:49 +0300

    A1 - C1 done

--- src/by/it/sc04_evening_tue_thu/krasikova/lesson07/TaskC1.java
+++ src/by/it/sc04_evening_tue_thu/krasikova/lesson07/TaskC1.java
@@ -10,14 +10,35 @@ package by.it.sc04_evening_tue_thu.krasikova.lesson07;
     Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
 */
 
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class TaskC1 {
-    public static void main(String[] args) throws Exception
-    {
+    public static void main(String[] args) throws Exception {
+        ArrayList<Integer> spisok = new ArrayList();
+        Scanner sc=new Scanner(System.in);
+        for (int i=0; i < 20; i++) {
+            int l = sc.nextInt();
+            spisok.add(l);}
+        ArrayList<Integer> spisok1 = new ArrayList();
+        ArrayList<Integer> spisok2 = new ArrayList();
+        ArrayList<Integer> spisok3 = new ArrayList();
+        for (int i=0; i< spisok.size(); i++) {
+            Integer x = spisok.get(i);
+            if (x%3==0) {
+                spisok1.add(x);
+            }
+            if (x%2==0) {
+                spisok2.add(x);
+            } if (x%3!=0&&x%2!=0) spisok3.add(x);
+            printList(spisok1);
+            printList(spisok2);
+            printList(spisok3);
+        }
+        }
 
-
-    }
 
     private static void printList(List<Integer> list) {
         for (Integer aList : list) System.out.println(aList);
