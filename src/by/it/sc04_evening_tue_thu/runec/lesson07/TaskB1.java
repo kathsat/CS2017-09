dbad754691da63ae3b5b5975691ef85ec74030cd
travel.vi@mail.ru <travel.vi@mail.ru>
2017-10-18 18:33:28 +0300

    Runets. Lesson 6, A1-B3

---
+++ src/by/it/sc04_evening_tue_thu/runec/lesson07/TaskB1.java
@@ -0,0 +1,30 @@
+package by.it.sc04_evening_tue_thu.runec.lesson07;
+/*
+Создайте 5 различных строчек в списке
+1. Создайте список строк.
+2. Добавьте в него 5 различных строчек.
+3. Выведите его размер на экран.
+4. Используя цикл выведите его содержимое на экран, каждое значение с новой строки.
+*/
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TaskB1 {
+    public static void main(String[] args) {
+        ArrayList<String> arr=new ArrayList<>();
+        Scanner s=new Scanner(System.in);
+        arr.add("Толпа глухая,");
+        arr.add("Крылатой новизны любовница слепая,");
+        arr.add("Надменных баловней меняет каждый день,");
+        arr.add("И катятся стуча с ступени на ступень");
+        arr.add("Кумиры их, вчера увенчанные ею.");
+        System.out.println(arr.size());
+        for (int i = 0; i < arr.size(); i++) {
+            System.out.println(arr.get(i));
+
+        }
+    }
+
+}
---
+++ src/by/it/sc04_evening_tue_thu/runec/lesson07/TaskC1.java
@@ -0,0 +1,50 @@
+package by.it.sc04_evening_tue_thu.runec.lesson07;
+/*
+Три массива
+1.  Введите с клавиатуры 20 чисел, сохраните их в список и рассортируйте по трём другим спискам:
+    Число делится на 3 (x%3==0), делится на 2 (x%2==0) и все остальные.
+    Числа, которые делятся на 3 и на 2 одновременно, например 6, попадают в оба списка.
+2.  Статический метод void printList(List<Integer> list) должен выводить на экран
+    все элементы переданного ему списка list, каждый элемент - с новой строки.
+3.  Используя метод printList выведите ваши три списка на экран.
+    Сначала тот, который для x%3, потом тот, который для x%2, потом последний.
+*/
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class TaskC1 {
+    public static void main(String[] args) throws Exception
+    {
+        Scanner s= new Scanner(System.in);
+        int data[] = new int[20];
+        for (int i = 0; i < data.length; i++) {
+            data[i]=s.nextInt();
+        }
+        ArrayList<Integer> arr=new ArrayList<>();
+        for (int i = 0; i <data.length ; i++) arr.add(data[i]);
+
+        ArrayList<Integer> three=new ArrayList<>();
+        ArrayList<Integer>two=new ArrayList<>();
+        ArrayList<Integer>another=new ArrayList<>();
+        for (int i = 0; i < arr.size(); i++) {
+            Integer x=arr.get(i);
+            if(x%3==0){
+                three.add(x);}
+            if (x%2==0){
+                two.add(x);}
+            if(!(x%3==0)&!(x%2==0)){
+                another.add(x);}}
+        printList(three);
+        printList(two);
+        printList(another);
+
+    }
+
+
+    private static void printList(List<Integer> list) {
+
+        for (Integer aList : list) System.out.println(aList); }
+
+}
