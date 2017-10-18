2e1770e0f82af9cd2e2568534982173918a52538
marina <sma_86@list.ru>
2017-10-19 01:06:49 +0300

    Lesson 7: A1-A2,B1 finished

--- src/by/it/sc02_morning/sinitsyna/lesson07/TaskB1.java
+++ src/by/it/sc02_morning/sinitsyna/lesson07/TaskB1.java
@@ -8,6 +8,19 @@ package by.it.sc02_morning.sinitsyna.lesson07;
 */
 
 
-public class TaskB1 {
+import java.util.ArrayList;
 
+public class TaskB1 {
+    public static void main(String[] args) {
+        ArrayList<String> atr = new ArrayList<>();
+        atr.add("H11");
+        atr.add("H12");
+        atr.add("H13");
+        atr.add("H14");
+        atr.add("H15");
+        System.out.println(atr.size());
+        for (int i = 0; i < atr.size(); i++) {
+            System.out.println(atr.get(i));
+        }
+    }
 }
