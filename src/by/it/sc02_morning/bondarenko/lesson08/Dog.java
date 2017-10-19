1c88dbdf983fc436f8575d7b621a8200cb649d25
NickoBond <nicko.bond@live.com>
2017-10-19 19:30:06 +0300

    Lesson 08 Sections A & B (with some leftover)

---
+++ src/by/it/sc02_morning/bondarenko/lesson08/Dog.java
@@ -0,0 +1,28 @@
+package by.it.sc02_morning.bondarenko.lesson08;
+
+public class Dog {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Кличка: %s. Возраст: %d", name, age);
+    }
+}
---
+++ src/by/it/sc02_morning/bondarenko/lesson08/DogHelper.java
@@ -0,0 +1,18 @@
+package by.it.sc02_morning.bondarenko.lesson08;
+
+class DogHelper {
+
+    static void printAllNames(Dog[] dogs) {
+        for (int i = 0; i < dogs.length; i++) {
+            System.out.printf(dogs[i].getName() + ((i == dogs.length - 1) ? "" : " "));
+        }
+    }
+
+    static double averageAge(Dog[] dogs) {
+        double sum = 0;
+        for (Dog dog : dogs) {
+            sum += dog.getAge();
+        }
+        return sum / dogs.length;
+    }
+}
