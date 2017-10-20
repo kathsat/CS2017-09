1c3e047a847f42233c24c33a0fc919094c316c53
Lizliza <gykish_liza@mail.ru>
2017-10-20 20:43:17 +0300

    a1

---
+++ src/by/it/sc03_evening_mon_fri/gukish/lesson08/Dog.java
@@ -0,0 +1,25 @@
+package by.it.sc03_evening_mon_fri.gukish.lesson08;
+
+public class Dog {
+
+    private String name;
+    private int age;
+
+    String getName(){
+        return name;
+    }
+    int getAge(){
+        return age;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+
+
+            void setAge( int age){
+            this.age = age;
+        }
+
+        }
