c0aef21fc1780579b1d49a3f7f83e7a35ad9ccce
halaevanika <halaeva.nika@gmail.com>
2017-10-20 20:42:18 +0300

    Lesson08

---
+++ src/by/it/sc03_evening_mon_fri/halaevanika/lesson08/Dog.java
@@ -0,0 +1,24 @@
+package by.it.sc03_evening_mon_fri.halaevanika.lesson08;
+
+public class Dog {
+
+    private String name;
+    private int age;
+
+    String getName() {
+        return name;
+
+    }
+
+    int getAge() {
+        return age;
+    }
+
+    void setName(String name){
+  this.name=name;
+}
+  void setAge(int age){
+      this.age=age;
+    }
+
+}
