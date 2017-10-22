1901501c947c33d9f57d1bad6ac617e01113a125
Marry <krawchinskaja@gmail.com>
2017-10-22 22:17:20 +0300

    A B C

--- src/by/it/sc03_evening_mon_fri/kravchinskaja_/lesson08/DogHelper.java
+++ src/by/it/sc03_evening_mon_fri/kravchinskaja_/lesson08/DogHelper.java
@@ -1,11 +1,17 @@
 package by.it.sc03_evening_mon_fri.kravchinskaja_.lesson08;
 
 public class DogHelper {
-    void printAllNames(Dog[] localdogs) {
+    static void printAllNames(Dog[] localdogs) {
         for (Dog d : localdogs)
            System.out.print(d.getName() + " ");
 System.out.println();
     }
-    static double averageAge(Dog[] doga)
-    double averageAge(Dog[] dogs)
-}
+    static double averageAge(Dog[] dogs){
+        double sum=0;
+        for(Dog d: dogs)
+            sum+=d.getAge();
+        return sum/dogs.length;
+
+
+    }
+    }
--- src/by/it/sc03_evening_mon_fri/kravchinskaja_/lesson08/TaskB1.java
+++ src/by/it/sc03_evening_mon_fri/kravchinskaja_/lesson08/TaskB1.java
@@ -28,31 +28,28 @@ double averageAge(Dog[] dogs){.....}; //должен вычислять сред
 public class TaskB1 {
     public static void main(String[] args) {
         Dog[] dogs=new Dog[5];
-        dogs[0]=new Dog;
+        dogs[0]=new Dog();
         dogs[0].setName("Шарик");
         dogs[0].setAge(1);
 
-        Dog[] dogs=new Dog[5];
-        dogs[1]=new Dog;
+        dogs[1]=new Dog();
         dogs[1].setName("Жучка");
         dogs[1].setAge(2);
 
-        Dog[] dogs=new Dog[5];
-        dogs[2]=new Dog;
+        dogs[2]=new Dog();
         dogs[2].setName("Бобик");
         dogs[2].setAge(3);
 
-        Dog[] dogs=new Dog[5];
-        dogs[3]=new Dog;
+        dogs[3]=new Dog();
         dogs[3].setName("Барбос");
         dogs[3].setAge(4);
 
-        Dog[] dogs=new Dog[5];
-        dogs[4]=new Dog;
+        dogs[4]=new Dog();
         dogs[4].setName("Полкан");
         dogs[4].setAge(5);
 
-       DogHelper
+       DogHelper.printAllNames(dogs);
+           double avAge=DogHelper.averageAge(dogs);
+           System.out.println(avAge);
     }
-
 }
